b02ab9cd55dc9b42a844b89c8cb31dfce6e2e828
penghan <penghan@yitu-inc.com>
2021-07-22 22:51:43 +0800

    multi-arch friendly

--- configs/tools/refresh_v2ray.sh
+++ configs/tools/refresh_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./v2gen_amd64_linux -u https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3 -o ./vcore/config.json -config v2gen_init.conf -best
+./v2gen -u https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3 -o ./vcore/config.json -config v2gen_init.conf -best
 systemctl --user restart v2ray
 
 
