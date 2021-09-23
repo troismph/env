0761cfd1d3c3c74f479cc0c7d89947dc48a37da7
g4z3 <orcishhan@gmail.com>
2021-09-23 11:36:17 +0800

    fix bugs in refresh_v2ray.sh

--- configs/tools/refresh_v2ray.sh
+++ configs/tools/refresh_v2ray.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-./v2gen -u https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3 -o ./vcore/config.json -config v2gen_init.conf -best
+BASEDIR=/home/g4z3/tools
+LOGFILE=/tmp/refresh_v2ray.log
+echo "refreshing v2ray config" >> $LOGFILE
+date >> $LOGFILE
+$BASEDIR/v2gen -u https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3 -o $BASEDIR/vcore/config.json -config $BASEDIR/v2gen_init.conf -best >> $LOGFILE 2>&1
+echo "restarting v2ray service" >> $LOGFILE
 systemctl --user restart v2ray
-
-
-
+date >> $LOGFILE
+echo "refresh done" >> $LOGFILE
