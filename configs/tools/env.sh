ae3099a91590e9b40568e4c3d060a8db3d64e7ba
g4z3 <orcishhan@gmail.com>
2021-09-23 11:57:47 +0800

    tidy up bashrc

--- configs/tools/env.sh
+++ configs/tools/env.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-export PATH=$PATH:~/tools/go/bin
+# golang settings
+export PATH=$PATH:~/tools:~/tools/go/bin
 export GOPATH=~/tools/go
 export GOPROXY="https://mirrors.aliyun.com/goproxy/"
 
+# project ec settings
 alias ecgit='GIT_SSH_COMMAND="ssh -i ~/.ssh/constgz_id_rsa " git'
+
+# python virtualenvwrapper settings
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/src
+source /usr/local/bin/virtualenvwrapper.sh
+
+# NVM settings
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
