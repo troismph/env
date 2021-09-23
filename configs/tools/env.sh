c37c4d18b3bb55529398c2501565532feffedd91
g4z3 <orcishhan@gmail.com>
2021-09-23 12:46:57 +0800

    pipx completions

--- configs/tools/env.sh
+++ configs/tools/env.sh
@@ -17,3 +17,6 @@ source /usr/local/bin/virtualenvwrapper.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# pipx settings
+eval "$(register-python-argcomplete pipx)"
