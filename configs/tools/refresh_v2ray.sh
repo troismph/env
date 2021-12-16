f1786e244b6919ce799647ece84083ff59264c8f
PH <hanpeng@probquant.com>
2021-12-16 18:05:25 +0800

    v2ray using template file

--- configs/tools/refresh_v2ray.sh
+++ configs/tools/refresh_v2ray.sh
@@ -4,7 +4,7 @@ BASEDIR=/home/g4z3/tools
 LOGFILE=/tmp/refresh_v2ray.log
 echo "refreshing v2ray config" >> $LOGFILE
 date >> $LOGFILE
-$BASEDIR/v2gen -u https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3 -o $BASEDIR/vcore/config.json -config $BASEDIR/v2gen_init.conf -best >> $LOGFILE 2>&1
+$BASEDIR/v2gen -template $BASEDIR/v2template.json -u https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3 -o $BASEDIR/vcore/config.json -config $BASEDIR/v2gen_init.conf -best >> $LOGFILE 2>&1
 echo "restarting v2ray service" >> $LOGFILE
 systemctl --user restart v2ray
 date >> $LOGFILE
