1d07b4f4d9446f040725d4bccda5602dd4c215e0
g4z3 <orcishhan@gmail.com>
2022-04-05 10:34:53 +0800

    misc updates

--- configs/tools/disable_tp_middle_button.sh
+++ configs/tools/disable_tp_middle_button.sh
@@ -3,7 +3,6 @@
 DT=`date`
 echo "$DT running tp script" >> /tmp/xinput
 DISPLAY=:0 /usr/bin/xinput set-button-map "ThinkPad Multi Connect Bluetooth Keyboard with Trackpoint Mouse" 1 0 3 4 5 6 7 &>> /tmp/xinput
-DEVID=`xinput list --id-only "pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint"`
-DISPLAY=:0 /usr/bin/xinput set-button-map $DEVID 1 0 3 4 5 6 7 &>> /tmp/xinput
+# DISPLAY=:0 DEVID=`xinput list --id-only "pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint"`
+DISPLAY=:0 /usr/bin/xinput set-button-map "pointer:Lenovo ThinkPad Compact USB Keyboard with TrackPoint" 1 0 3 4 5 6 7 &>> /tmp/xinput
 DISPLAY=:0 /usr/bin/xinput set-button-map "TPPS/2 Elan TrackPoint" 1 0 3 4 5 6 7 &>> /tmp/xinput
-
--- configs/tools/tp_button_unlock.sh
+++ configs/tools/tp_button_unlock.sh
@@ -1,7 +1,16 @@
-/usr/bin/dbus-monitor --session "type='signal',interface='org.xfce.ScreenSaver'" |
+# /usr/bin/dbus-monitor --session "type='signal',interface='org.xfce.ScreenSaver'" |
+#     while read x; do
+#         case "$x" in
+#             *"boolean true"*) /bin/echo SCREEN_LOCKED >> /tmp/xinput;;
+#             *"boolean false"*) $HOME/tools/disable_tp_middle_button.sh;;
+#         esac
+#     done &
+/usr/bin/dbus-monitor --session "type='signal',interface='org.blueman.Applet',member='VisibilityChanged'" |
     while read x; do
+        DT=`/usr/bin/date`
         case "$x" in
-            *"boolean true"*) /bin/echo SCREEN_LOCKED >> /tmp/xinput;;
-            *"boolean false"*) $HOME/tools/disable_tp_middle_button.sh;;
+            *"boolean true"*) sleep 3; /bin/echo "$DT" >> /tmp/xinput; $HOME/tools/disable_tp_middle_button.sh;;
+            *"boolean false"*) sleep 3; /bin/echo "$DT" >> /tmp/xinput; $HOME/tools/disable_tp_middle_button.sh;;
+            #*"boolean false"*) $HOME/tools/disable_tp_middle_button.sh;;
         esac
     done &
