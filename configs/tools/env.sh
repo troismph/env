fd305642685b9cdc991d9ed29f0b768272f7a2b5
PH <hanpeng@probquant.com>
2022-06-25 17:40:19 +0800

    Misc updates
    - Emacsd IM switched to ibus
    - add & enabled dropbox service
    - fix bug in env.sh

--- configs/tools/env.sh
+++ configs/tools/env.sh
@@ -11,7 +11,7 @@ alias ecgit='GIT_SSH_COMMAND="ssh -i ~/.ssh/constgz_id_rsa " git'
 # python virtualenvwrapper settings
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/src
-source /usr/local/bin/virtualenvwrapper.sh
+source $HOME/.local/bin/virtualenvwrapper.sh
 
 # NVM settings
 export NVM_DIR="$HOME/.nvm"
@@ -19,4 +19,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # pipx settings
-eval "$(register-python-argcomplete pipx)"
+eval "$(register-python-argcomplete3 pipx)"
