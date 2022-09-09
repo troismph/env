0480c1dbd22db4f196beb608ee33dd4363dcec86
PH <hanpeng@probquant.com>
2022-09-09 14:20:12 +0800

    misc

--- configs/tools/refresh_v2ray.sh
+++ configs/tools/refresh_v2ray.sh
@@ -4,6 +4,7 @@ SUBSCRIPTION=https://my.yunti.monster/link/WbBgi0t0QXornuCZ?sub=3
 BASEDIR=~/tools
 LOGFILE=/tmp/refresh_v2ray.log
 CONFIG_OUTPUT=$BASEDIR/vcore/config.json
+CONFIG_OUTPUT_ALL_PROXY=$BASEDIR/vcore/config_all_proxy.json
 RETRY_INTERVAL=15
 echo "refreshing v2ray config" >> $LOGFILE
 date >> $LOGFILE
@@ -11,6 +12,7 @@ CK="0"
 while [ $CK -eq "0" ]
 do
     $BASEDIR/v2gen -template $BASEDIR/v2template.json -u $SUBSCRIPTION -o $CONFIG_OUTPUT -config $BASEDIR/v2gen_init.conf -best -thread 50 >> $LOGFILE 2>&1
+    #$BASEDIR/v2gen -template $BASEDIR/v2template_all_proxy.json -u $SUBSCRIPTION -o $CONFIG_OUTPUT_ALL_PROXY -config $BASEDIR/v2gen_init_all_proxy.conf -best -thread 50 >> $LOGFILE 2>&1
     CK=`jq '.outbounds[] | select( .protocol == "vmess" ) | .settings.vnext[0].port' < $CONFIG_OUTPUT | grep -E "[0-9]+" | wc -l`
     if [ $CK -eq "0" ]
     then
