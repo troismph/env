805360d9bc888c69967416ce4d7ac63bdcdacb96
PH <hanpeng@probquant.com>
2022-12-09 13:19:39 +0800

    misc

--- configs/tools/create_mobile_proxy.sh
+++ configs/tools/create_mobile_proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-autossh -L 127.0.0.1:20080:localhost:12080 g4z3@tread.miniogre.com -N
+#autossh -L 127.0.0.1:20080:localhost:12080 g4z3@tread.miniogre.com -N
+autossh -D 12080 -N -C g4z3-dt
