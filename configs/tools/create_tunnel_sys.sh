e66fa9df92160364ed2ea2c98d679d21ebae8e7a
PH <ph@alphaith.com>
2024-04-05 11:36:40 +0800

    misc updates

--- configs/tools/create_tunnel_sys.sh
+++ configs/tools/create_tunnel_sys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# PID_FILE="/tmp/tunnel_sys.pid"
-# if [ -f $PID_FILE ]; then
-#     echo "Terminating existing tunnel..."
-#     pkill -F $PID_FILE
-# fi
-# AUTOSSH_PIDFILE=$PID_FILE autossh -i /home/ph/.ssh/ph -N -R 10026:localhost:22 g4z3@tread.miniogre.com
-ssh -i /home/ph/.ssh/ph -N -R 10026:localhost:22 g4z3@tread.miniogre.com
+PID_FILE="/tmp/tunnel_sys.pid"
+if [ -f $PID_FILE ]; then
+    echo "Terminating existing tunnel..."
+    pkill -F $PID_FILE
+fi
+AUTOSSH_PIDFILE=$PID_FILE autossh -f -i /home/ph/.ssh/ph -N -R 10026:localhost:22 g4z3@tread.miniogre.com
+# ssh -i /home/ph/.ssh/ph -N -R 10026:localhost:22 g4z3@tread.miniogre.com
