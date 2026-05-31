cb296117bc091fe83fa43a37aacbf1d565db44cb
PH <ph@alphaith.com>
2026-05-31 23:14:22 +0800

    update env.sh

--- configs/tools/env.sh
+++ configs/tools/env.sh
@@ -6,12 +6,12 @@ export GOPATH=~/tools/go
 export GOPROXY="https://mirrors.aliyun.com/goproxy/"
 
 # project ec settings
-alias ecgit='GIT_SSH_COMMAND="ssh -i ~/.ssh/constgz_id_rsa " git'
+# alias ecgit='GIT_SSH_COMMAND="ssh -i ~/.ssh/constgz_id_rsa " git'
 
 # python virtualenvwrapper settings
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/src
-source $HOME/.local/bin/virtualenvwrapper.sh
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
 # NVM settings
 export NVM_DIR="$HOME/.nvm"
@@ -19,4 +19,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # pipx settings
-eval "$(register-python-argcomplete3 pipx)"
+# eval "$(register-python-argcomplete3 pipx)"
